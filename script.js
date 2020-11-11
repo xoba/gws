8196a8220f19b8932667c166bb1cf11cae7b0d5b
mike andrews <mra@xoba.com>
2020-11-11 14:20:00 -0500

    clean up the js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var exampleSocket = new WebSocket("ws://"+ location.host +"/t")
-
-exampleSocket.onopen = function (event) {
-    console.log("open");
-};
-
-
-exampleSocket.onmessage = function (event) {
-    console.log("got: " + event.data);
-    exampleSocket.send(JSON.stringify("thanks for " + event.data));
-}
+(function() {
+    var exampleSocket = new WebSocket("ws://"+ location.host +"/t")
+    exampleSocket.onopen = function (event) {
+	console.log("opened websocket");
+    };
+    exampleSocket.onmessage = function (event) {
+	console.log("got: " + event.data);
+	exampleSocket.send(JSON.stringify("thanks for " + event.data));
+    }    
+})()
